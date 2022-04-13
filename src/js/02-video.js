171f3e75431afe55e15bc1b9f45437d0548f80b8
RomaSamkov <Romansamkov.rs@gmail.com>
2022-04-13 19:28:26 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+const onPlay = function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(currentTime)
+    .then(function (seconds) { })
+    .catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
