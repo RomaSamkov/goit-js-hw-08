728ce5dcfc520ec3f8046305fce1b94299a1470e
RomaSamkov <Romansamkov.rs@gmail.com>
2022-04-13 17:11:19 +0300

    add src folder

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,32 +1,5 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-
-const galleryImagesContainerEl = document.querySelector('.gallery');
-const itemsMurkup = createItemsImagesMurkup(galleryItems);
-galleryImagesContainerEl.insertAdjacentHTML('beforeend', itemsMurkup);
-
-
-function createItemsImagesMurkup(item) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
-      <a class="gallery__link" href="${original.value}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </div>`
-    }).join('');
-} 
-
-const lightBox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
+console.log(galleryItems);
