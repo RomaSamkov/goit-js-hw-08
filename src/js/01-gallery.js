85005766c42f15f4cf7f39f28246d24007d3b8e7
RomaSamkov <Romansamkov.rs@gmail.com>
2022-04-13 15:32:14 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryImagesContainerEl = document.querySelector('.gallery');
+const itemsMurkup = createItemsImagesMurkup(galleryItems);
+galleryImagesContainerEl.insertAdjacentHTML('beforeend', itemsMurkup);
+
+
+function createItemsImagesMurkup(item) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+      <a class="gallery__link" href="${original.value}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`
+    }).join('');
+} 
+
+const lightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
